7087bd71224971e6a65e2853daf5b6901f38d951
lechebs <lucaballone02@gmail.com>
2025-12-28 20:41:28 +0100

    formatting convergence table for global convergence test, added (failed) global time convergence test

--- tests/convergence-test.c
+++ tests/convergence-test.c
@@ -6,6 +6,7 @@
 #include "boundary.h"
 #include "consts.h"
 #include "solver.h"
+#include "convergence-test.h"
 
 #define POW2(x) ((x) * (x))
 
@@ -391,72 +392,6 @@ static double field_l2_dist(field_size size, const_field f1, const_field f2)
     return sqrt(dist * _DX * _DX * _DX);
 }
 
-static double field3_l2_dist(field_size size, const_field3 f1, const_field3 f2)
-{
-    double dist = 0;
-
-    /*
-    printf("\n\nreference:");
-
-    for (uint32_t i = 0; i < size.depth;  ++i) {
-        printf("\n");
-        for (uint32_t j = 0; j < size.height; ++j) {
-            printf("\n");
-            for (uint32_t k = 0; k < size.width; ++k) {
-
-                uint64_t idx = size.height * size.width * i +
-                               size.width * j + k;
-
-                printf("%g ", f1.x[idx]);
-            }
-        }
-    }
-
-    printf("\n\nsolution:");
-
-    for (uint32_t i = 0; i < size.depth;  ++i) {
-        printf("\n");
-        for (uint32_t j = 0; j < size.height; ++j) {
-            printf("\n");
-            for (uint32_t k = 0; k < size.width; ++k) {
-
-                uint64_t idx = size.height * size.width * i +
-                               size.width * j + k;
-
-                printf("%g ", f2.x[idx]);
-            }
-        }
-    }
-
-    printf("\n\nsolution error:");
-    */
-
-    for (uint32_t i = 0; i < size.depth; ++i) {
-        //printf("\n");
-        for (uint32_t j = 0; j < size.height; ++j) {
-            //printf("\n");
-            for (uint32_t k = 0; k < size.width; ++k) {
-
-                uint64_t idx = size.height * size.width * i +
-                               size.width * j + k;
-
-                /* WARNING: You need to multiply by dx^3!
-                 * You're computing a norm! */
-
-                double err = (POW2(f1.x[idx] - f2.x[idx]) +
-                              POW2(f1.y[idx] - f2.y[idx]) +
-                              POW2(f1.z[idx] - f2.z[idx])) * _DX * _DX * _DX;
-
-                //printf("%g ", fabs(((f1.x[idx] - f2.x[idx]) / fmax(f1.x[idx], 1e-12))));
-
-                dist += err;
-            }
-        }
-    }
-
-    return sqrt(dist);
-}
-
 static void compute_manufactured_velocity(field_size size,
                                           uint32_t timestep,
                                           field3 dst)
@@ -490,15 +425,20 @@ static void compute_manufactured_pressure(field_size size,
                                           uint32_t timestep,
                                           field dst)
 {
+    /* WARNING: Pressure is computed at t + dt/2 */
+    double time = timestep * _DT;
+
     for (uint32_t i = 0; i < size.depth; ++i) {
         for (uint32_t j = 0; j < size.height; ++j) {
             for (uint32_t k = 0; k < size.width; ++k) {
                 uint64_t idx = size.height * size.width * i +
                                size.width * j + k;
 
-                dst[idx] = 0.0;//-3.0 * _NU * sin(time) * cos(k * _DX)
-                               //       * sin(j * _DX)
-                               //       * (sin(i * _DX) - cos(i * _DX));
+                /*
+                    dst[idx] = sin(time) * sin(k * _DX) * sin(j * _DX) *
+                               sin(i * _DX);
+                */
+                dst[idx] = 0;
             }
         }
     }
@@ -506,9 +446,9 @@ static void compute_manufactured_pressure(field_size size,
 
 DEF_TEST(test_manufactured_convergence_space,
          ArenaAllocator *arena,
-         uint32_t max_depth,
-         uint32_t max_height,
-         uint32_t max_width,
+         uint32_t min_depth,
+         uint32_t min_height,
+         uint32_t min_width,
          int num_timesteps,
          int num_samples)
 {
@@ -516,17 +456,19 @@ DEF_TEST(test_manufactured_convergence_space,
 
     double *velocity_errors = arena_push_count(arena, double, num_samples);
     double *pressure_errors = arena_push_count(arena, double, num_samples);
+    double *dxs = arena_push_count(arena, double, num_samples);
 
-    SET_DT(0.0000001);
+    SET_DT(1e-6);
 
     for (int i = 0; i < num_samples; ++i) {
         arena_enter(arena);
 
-        field_size size = { max_width >> i,
-                            max_height >> i,
-                            max_depth >> i };
+        field_size size = { min_width << i,
+                            min_height << i,
+                            min_depth << i };
 
         SET_DX(1.0 / size.width);
+        dxs[i] = _DX;
 
         Solver *solver = solver_alloc(size.depth, size.height,
                                       size.width, arena);
@@ -543,7 +485,8 @@ DEF_TEST(test_manufactured_convergence_space,
         compute_manufactured_velocity(size, num_timesteps, ref_velocity);
         compute_manufactured_pressure(size, num_timesteps, ref_pressure);
 
-        velocity_errors[i] = field3_l2_dist(size,
+        velocity_errors[i] = field3_l2_norm_diff(size,
+                                            _DX,
                                             to_const_field3(ref_velocity),
                                             solver_get_velocity(solver));
 
@@ -554,12 +497,96 @@ DEF_TEST(test_manufactured_convergence_space,
         arena_exit(arena);
     }
 
-    for (int i = 0; i < num_samples; ++i) {
-        printf("%3d u=%g p=%g\n", max_width >> i,
-               velocity_errors[i], pressure_errors[i]);
+    double *velocity_orders = arena_push_count(arena, double, num_samples);
+    double *pressure_orders = arena_push_count(arena, double, num_samples);
+    estimate_convergence_order(velocity_errors, dxs,
+                               num_samples, velocity_orders);
+    estimate_convergence_order(pressure_errors, dxs,
+                               num_samples, pressure_orders);
+
+    printf("%3d %12g  --  %12g  --\n", min_width,
+           velocity_errors[0], pressure_errors[0]);
+    for (int i = 1; i < num_samples; ++i) {
+        printf("%3d %12g %.2f %12g %.2f\n", min_width << i,
+               velocity_errors[i], velocity_orders[i],
+               pressure_errors[i], pressure_orders[i]);
+        EXPECT_EQUALF(velocity_orders[i], 2.0, 0.1);
+        EXPECT_EQUALF(pressure_orders[i], 2.0, 0.1);
     }
 
-    FAIL_TEST();
+    arena_exit(arena);
+}
+
+DEF_TEST(test_manufactured_convergence_time,
+         ArenaAllocator *arena,
+         int num_samples)
+{
+    arena_enter(arena);
+
+    double *velocity_errors = arena_push_count(arena, double, num_samples);
+    double *pressure_errors = arena_push_count(arena, double, num_samples);
+    double *dts = arena_push_count(arena, double, num_samples);
+
+    field_size size = { 64, 64, 64 };
+    SET_DX(1.0 / size.width);
+
+    double T = 1.0;
+    double dt = 0.1;
+
+    Solver *solver = solver_alloc(size.depth, size.height,
+                                  size.width, arena);
+
+    for (int i = 0; i < num_samples; ++i) {
+        arena_enter(arena);
+
+        SET_DT(dt);
+        dts[i] = _DT;
+
+        solver_init(solver);
+
+        /* TODO: Set porosity. */
+
+        uint32_t num_timesteps = T / _DT;
+        printf("%d %f\n", num_timesteps, _DT);
+        for (uint32_t t = 1; t < num_timesteps + 1; ++t) {
+            solver_step(solver, t);
+        }
+
+        field3 ref_velocity = field3_alloc(size, arena);
+        field ref_pressure = field_alloc(size, arena);
+        compute_manufactured_velocity(size, num_timesteps, ref_velocity);
+        compute_manufactured_pressure(size, num_timesteps, ref_pressure);
+
+        velocity_errors[i] = field3_l2_norm_diff(size,
+                                                 _DX,
+                                                 to_const_field3(ref_velocity),
+                                                 solver_get_velocity(solver));
+
+        pressure_errors[i] = field_l2_dist(size,
+                                           ref_pressure,
+                                           solver_get_pressure(solver));
+
+        dt /= 2;
+
+        arena_exit(arena);
+    }
+
+    double *velocity_orders = arena_push_count(arena, double, num_samples);
+    double *pressure_orders = arena_push_count(arena, double, num_samples);
+    estimate_convergence_order(velocity_errors, dts,
+                               num_samples, velocity_orders);
+    estimate_convergence_order(pressure_errors, dts,
+                               num_samples, pressure_orders);
+
+    printf("%.8f %e  --  %e  --\n", dts[0],
+           velocity_errors[0], pressure_errors[0]);
+    for (int i = 1; i < num_samples; ++i) {
+        printf("%.8f %e %5.2f %e %5.2f\n", dts[i],
+               velocity_errors[i], velocity_orders[i],
+               pressure_errors[i], pressure_orders[i]);
+        EXPECT_EQUALF(velocity_orders[i], 2.0, 0.1);
+        EXPECT_EQUALF(pressure_orders[i], 2.0, 0.1);
+    }
 
     arena_exit(arena);
 }
@@ -569,7 +596,8 @@ int main(void)
     ArenaAllocator arena;
     arena_init(&arena, 1ul << 32);
 
-    RUN_TEST(test_manufactured_convergence_space, &arena, 256, 256, 256, 5, 5);
+    RUN_TEST(test_manufactured_convergence_space, &arena, 8, 8, 8, 10, 4);
+    RUN_TEST(test_manufactured_convergence_time, &arena, 4);
 
     arena_destroy(&arena);
 
